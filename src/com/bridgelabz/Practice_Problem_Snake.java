e6801982144f6f8d294726d7ffe17c23f4f8f6b4
Kisalay <kisalaykisu@gmail.com>
2022-06-19 22:13:31 +0530

    first commit

---
+++ src/com/bridgelabz/Practice_Problem_Snake.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+
+
+public class Practice_Problem_Snake {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the game of mind snake and ladder.Lets play together.");
+    }
+}
