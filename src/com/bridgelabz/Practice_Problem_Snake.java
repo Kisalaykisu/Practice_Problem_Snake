a0cfbe6fb06770d30d7a4285a5537150ba2a6bfb
Kisalay <kisalaykisu@gmail.com>
2022-06-19 22:26:25 +0530

    3rd commit

--- src/com/bridgelabz/Practice_Problem_Snake.java
+++ src/com/bridgelabz/Practice_Problem_Snake.java
@@ -1,6 +1,7 @@
-/* Q-1) Snake and Ladder game played with
-        single player at start
-        position 0 .
+/* Q-2) The Player rolls the die
+        to get a number
+        between 1 to 6. - Use ((RANDOM)) to get the number between
+        1 to 6
 */
 
 package com.bridgelabz;
@@ -8,8 +9,18 @@ package com.bridgelabz;
 import java.util.Random;  // import random
 
 public class Practice_Problem_Snake {
-    public static void main(String[] args) {
+    public static void main(String[] args) {  //entry point of program
 
-        int position = 0;    // Starting position is at 0
+        int position = 0;
+        int dieRolls = 0;
+
+        Random rand = new Random();  //Random object initializer
+
+        while (position < 100) {
+            int dice = rand.nextInt(6) + 1;
+            dieRolls += 1;
+            position += dice;   //Actual position
+            System.out.println("Die number is: "+dice);
+        }
     }
 }
